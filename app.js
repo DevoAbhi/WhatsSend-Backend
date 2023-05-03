c0045a1a0a3d336a1e6908eda86c052541960d65
AbhiDroid <abhinabroy2001@gmail.com>
2023-05-03 11:10:14 +0530

    checking

--- app.js
+++ app.js
@@ -4,11 +4,7 @@ const path = require("path");
 const cors = require('cors');
 
 const app = express();
-app.use(
-    cors({
-      origin: "*",
-    })
-);
+app.use(cors());
 
 //route
 const whatsappRoute = require("./routes/whatsapp");
@@ -17,13 +13,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept, Authorization, X-AUTH-TOKEN");
-    res.setHeader("Access-Control-Allow-Methods","GET, POST, PATCH,PUT, DELETE,OPTIONS");
+// app.use((req, res, next) => {
+//     res.setHeader("Access-Control-Allow-Origin", "*");
+//     res.setHeader("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept, Authorization, X-AUTH-TOKEN");
+//     res.setHeader("Access-Control-Allow-Methods","GET, POST, PATCH,PUT, DELETE,OPTIONS");
 
-    next();
-});
+//     next();
+// });
 
 app.use(whatsappRoute);
 
